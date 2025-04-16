88313cf63e9196286c6120c59e5da6e0fbb816a3
TechGuyy1 <techguys3008gmail.com>
2025-04-16 12:17:51 +0530

    Comments added

--- ForLoopDemo.java
+++ ForLoopDemo.java
@@ -2,17 +2,17 @@ package basicjava;
 
 public class ForLoopDemo {
     public static void main(String[] args) {
-        int anArray[]={4,6,3,1};
-        //Print all ele of anArray using for looop
-
-//
-        for(int i=0;i<=anArray.length-1;i++){
+        int anArray[] = {4, 6, 3, 1};
+        //Print all ele of anArray using for loop
+        //Systax
+        //for(initialize the count;Add condition;increment the count)
+        for (int i = 0; i <= anArray.length - 1; i++) {
 //            System.out.print("Element at index :"+i +"is ");
 //            System.out.println(anArray[i]);
-            if(i==2){
-               break;
-            }else {
-                System.out.print("Element at index :"+i + "is:");
+            if (i == 2) {
+                break;
+            } else {
+                System.out.print("Element at index :" + i + "is:");
                 System.out.println(anArray[i]);
             }
 
