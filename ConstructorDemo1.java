8552daaac374c22bd2bd74b14fe21c7a4ff7882f
TechGuyy1 <techguys3008gmail.com>
2025-04-21 11:03:13 +0530

    Comments added

--- ConstructorDemo1.java
+++ ConstructorDemo1.java
@@ -1,27 +1,19 @@
 package basicjava;
 
 public class ConstructorDemo1 {
-//To initialize an object
+    //To initialize an object
     //Its name should be same as like a class name
-//Default Constructor
-    //java provide implicit cons
-public  ConstructorDemo1(){
-    System.out.println("Im in constructor of class "); //Explicit
-}
-   public void disPalay(){
-       System.out.println("I'm in reguler/normal method");
-
-   }
-
-
-   //Parametrized
-
-
-    public static void main(String[] args) {
-
-       ConstructorDemo1 obj1=new ConstructorDemo1();
-       obj1.disPalay();
-
+    //Default Constructor
+    //java provide implicit constructor when no explicit constructor defined
+    public ConstructorDemo1() {
+        System.out.println("I'm in explicit default constructor of class"); //Explicit
     }
-
+    public void display() {
+        System.out.println("I'm in display method");
+    }
+    //Parametrized
+    public static void main(String[] args) {
+        ConstructorDemo1 obj1 = new ConstructorDemo1();
+        obj1.display();
+    }
 }
--- StudentInfo.java
+++ StudentInfo.java
@@ -1,31 +1,28 @@
 package basicjava;
 
 public class StudentInfo {
-
     //Parametrize Constructor
 public String name;
 public  int marks;
    public StudentInfo(String name,int marks){
-     this.name=name;
-     this.marks=marks;
-       System.out.println("Student Name is:"+name);
-       System.out.println("and Marks: " + marks);
+       this.name=name;
+       this.marks=marks;
+       System.out.println("Student Name is:"+name + " and Marks: " + marks);
     }
     public void displayStudentdata(){
-        System.out.println("Student Name is:"+name);
-        System.out.println("and Marks: " + marks);
+        System.out.println("In displayStudentdata--> Student Name is:"+name + " and Marks: " + marks);
     }
     //Copy constructor
-
-
-
-
-
+    public StudentInfo(StudentInfo copy){
+        this.name= copy.name;
+        this.marks=copy.marks;
+        System.out.println(" Copying object --> Student Name is:"+name + " and Marks: " + marks);
+    }
     public static void main(String[] args) {
-        StudentInfo obj=new StudentInfo("Pavan Kumar",11);//Selenium framework
+        StudentInfo obj=new StudentInfo("Pavan Kumar",11);
+        StudentInfo obj1=new StudentInfo("Rohit Sharma",13);
         obj.displayStudentdata();
-//        System.out.println(obj.displayStudentdata());
-
-
+        //Initialize copy constructor
+        StudentInfo obj2=new StudentInfo(obj1);
     }
 }
