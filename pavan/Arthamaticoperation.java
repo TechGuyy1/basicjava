4dbcaeb04334ee530bc6d7a7fa7b61fada682167
metikalapavankumar <metikalapavankumar2@gmail.com>
2025-05-23 09:57:39 +0530

    Arthamaticoperation

---
+++ pavan/Arthamaticoperation.java
@@ -0,0 +1,10 @@
+package basicjava.pavan;
+
+public class Arthamaticoperation {
+    public static void main(String[] args) {
+        int a=1;
+        int b =2;
+        int add =a+b;
+        System.out.println("addition of two numbers:"+add);
+    }
+}
