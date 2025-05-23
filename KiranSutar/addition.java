dd5066a53c0394e8c5b893d911952714d55ab398
kiranrsutar8347@gmail.com <kiranrsutar8347@gmail.com>
2025-05-23 10:27:36 +0530

    addition

---
+++ KiranSutar/addition.java
@@ -0,0 +1,13 @@
+package KiranSutar;
+
+public class addition {
+
+    public static void main(String args[]) {
+
+
+        int a = 1;
+        int b = 1;
+        int sum = a + b;
+        System.out.println("addition = " +sum);
+    }
+}
